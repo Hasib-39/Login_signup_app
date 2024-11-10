939bd050bd7dff8fec25cc97e4245f6ba744ab73
Hasib Altaf <hasibaltaf@iut-dhaka.edu>
2024-11-10 15:08:16 +0600

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:login_signup/login.dart';
+
+import 'login.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login_Signup App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true
+      ),
+      home: Login(),
+    );
+  }
+}
+
+
